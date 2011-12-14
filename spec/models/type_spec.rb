d4f7f6bfa75dcf3e74e19b255a42aad5c0d32352
Peter Welte <weltepe@gmail.com>
2011-12-13 17:59:42 -0800

    fixed migration (added join tables)

--- spec/models/type_spec.rb
+++ spec/models/type_spec.rb
@@ -10,6 +10,15 @@ describe Type do
 	  specify {@type.should be_valid}
   end
 
+  # describe "when relating types and events" do
+  #   before(:each) do
+  #     @type = Type.new(:name => "protest")
+  #     @event = Event.new(:title => "Event title")
+  #   end
+
+  #   it "should find "
+
+
   describe "when creating nameless types" do
 	  before(:each) do
 	    @type = Type.new()
