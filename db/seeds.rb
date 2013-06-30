f25841ae40a550f8b9fe4e55126f15d4b0a150aa
Jordan Skarbek <jaskarbek@gmail.com>
2013-06-30 12:34:30 -0700

    update seed file to create a site and user. add dynamic site_id to topics and types.

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,34 +3,47 @@
 #
 # Examples:
 #
-#   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
-#   Mayor.create(:name => 'Daley', :city => cities.first)
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Daley', :city => cities.first)
+
+Site.destroy_all
+User.destroy_all
+Type.destroy_all
+Topic.destroy_all
+
+site = Site.create({ name: "activate_dev", domain: "localhost" })
+
+
+User.create({ email: "dev@example.org", password: "activate" })
 
 Type.create(
- [ 
-   { :name => 'volunteer' }, { :name => 'social' },
-   { :name => 'meeting' }, { :name => 'educational'},
-   { :name => 'rally'}, { :name => 'other' },
-   { :name => 'film screening'}
+ [
+   { name: 'volunteer', site_id: site.id },
+   { name: 'social', site_id: site.id },
+   { name: 'meeting', site_id: site.id },
+   { name: 'educational', site_id: site.id },
+   { name: 'rally', site_id: site.id },
+   { name: 'other', site_id: site.id },
+   { name: 'film screening', site_id: site.id }
  ]);
 
  Topic.create(
   [
-   { :name => 'animal rights' },
-   { :name => 'arts'},
-   { :name => 'biking' },
-   { :name => 'business'},
-   { :name => 'community'}, 
-   { :name => 'urban'},
-   { :name => 'environment'},
-   { :name => 'economics'},
-   { :name => 'food'},
-   { :name => 'health'},
-   { :name => 'human rights'},
-   { :name => 'politics'},
-   { :name => 'transportation'},
-   { :name => 'walking'},
-   { :name => 'transit'},
-   { :name => 'humanitarian aid'},
-   { :name => 'war and peace'},
+   { name: 'animal rights', site_id: site.id },
+   { name: 'arts', site_id: site.id  },
+   { name: 'biking', site_id: site.id },
+   { name: 'business', site_id: site.id },
+   { name: 'community', site_id: site.id },
+   { name: 'urban', site_id: site.id },
+   { name: 'environment', site_id: site.id },
+   { name: 'economics', site_id: site.id },
+   { name: 'food', site_id: site.id },
+   { name: 'health', site_id: site.id },
+   { name: 'human rights', site_id: site.id },
+   { name: 'politics', site_id: site.id },
+   { name: 'transportation', site_id: site.id },
+   { name: 'walking', site_id: site.id },
+   { name: 'transit', site_id: site.id },
+   { name: 'humanitarian aid', site_id: site.id },
+   { name: 'war and peace', site_id: site.id },
   ]);
