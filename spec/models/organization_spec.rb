2f3de5f74796235fcef8cc822e8a18a18287a1f6
Daniel Hedlund <daniel@digitree.org>
2013-07-03 23:55:17 -0700

    Removed org test that's not relevant anymore (create w/o name or email)
    Validation was recently added at the hackathon to require name and email
    email address when creating organizations; this is in direct conflict
    with the test being removed and I have no idea why it was added or is
    relevant as it's not associated with a commit that needs it.  Will find
    out later when working on import logic if a real requirement.

--- spec/models/organization_spec.rb
+++ spec/models/organization_spec.rb
@@ -4,8 +4,6 @@ describe Organization do
   subject(:organization) { Factory.build(:organization) }
 
   # Tests for Organization:
-  # - We should be able to create a basic org instance given just a name.
-  #  -- URL s/be null.
   # - Creating an organization with 'viagra' as name should fail.
   # - Creating an organization without a name is not possible.
   # - If the email is included, it should be valid
@@ -15,13 +13,6 @@ describe Organization do
   describe "in general" do
     before(:each) do
       @org = Organization.new(:name => "My Organization", :contact_name => "Juana", :email => "juana@myorganization.org")
-    end
-    specify {@org.should be_valid}
-  end
-
-  describe "when creating an organization without email or contact_name" do
-    before(:each) do
-       @org = Organization.new(:name => "My Organization")
     end
     specify {@org.should be_valid}
   end
