479d5baffe652c932b54ce716074d979d5ce53b9
Daniel Hedlund <daniel@digitree.org>
2013-07-06 19:19:32 -0700

    Rework db seeds file, can now work w/ init prod deploy

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,78 +3,60 @@
 #
 # Examples:
 #
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Daley', :city => cities.first)
+#   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
+#   Mayor.create(:name => 'Daley', :city => cities.first)
 require 'date'
 require 'faker'
 
 if Rails.env.development?
-   User.create!({ email: "dev@example.org", password: "activate" })
+  User.create!({ :email => "dev@example.org", :password => "activate" })
+
+  # Creates 20 random venue, organizations, and event database entries
+  site = Site.create!({ :name => "activate_dev", :domain => "localhost" })
+  1.upto(20) do
+    venue = Venue.create!({
+      :title       => Faker::Company::name,
+      :description => Faker::Lorem::paragraph(sentence_count=2),
+      :postal_code => Faker::Address::zip_code,
+      :country     => Faker::Address::country,
+      :email       => Faker::Internet::safe_email,
+      :telephone   => Faker::PhoneNumber::phone_number,
+      :site_id     => site.id
+    })
+
+    Organization.create!({
+      :name         => Faker::Company::name,
+      :url          => Faker::Internet::url,
+      :contact_name => Faker::Name::name,
+      :email        => Faker::Internet::safe_email,
+      :site_id      => site.id
+    })
+
+    Event.create!({
+      :title       => Faker::Lorem::sentence(word_count=2),
+      :description => Faker::Lorem::paragraph(sentence_count=2),
+      :start_time  => DateTime.now + rand(15),
+      :venue_id    => venue.id,
+      :site_id     => site.id
+    })
+
+    sleep 1 # FIXME: find a work around to geocoding API limits
+  end
 end
 
-site = Site.create!({ name: "activate_dev", domain: "localhost" })
+types = [
+  'volunteer', 'social', 'meeting', 'educational', 'rally',
+  'film screening', 'other'
+]
 
-# Creates 20 random venue, organizations, and event database entries
-counter = 20
+topics = [
+  'animal rights', 'arts', 'biking', 'business', 'community', 'urban',
+  'environment', 'economics', 'food', 'health', 'human rights', 'politics',
+  'transportation', 'walking', 'transit', 'humanitarian aid',
+  'war and peace'
+]
 
-while counter > 0
-   venue = Venue.create!({
-      title: Faker::Company::name,
-      description: Faker::Lorem::paragraph(sentence_count=2),
-      postal_code: Faker::Address::zip_code,
-      country: Faker::Address::country,
-      email: Faker::Internet::safe_email,
-      telephone: Faker::PhoneNumber::phone_number,
-      site_id: site.id
-   })
-
-   Organization.create!({
-      name: Faker::Company::name,
-      url: Faker::Internet::url,
-      contact_name: Faker::Name::name,
-      email: Faker::Internet::safe_email,
-      site_id: site.id
-   })
-
-   Event.create!({
-      title: Faker::Lorem::sentence(word_count=2),
-      description: Faker::Lorem::paragraph(sentence_count=2),
-      start_time: DateTime.now + rand(15),
-      venue_id: venue.id,
-      site_id: site.id
-   })
-   counter -= 1
-   sleep 1 # Prevents hitting API call limit
+Site.scoped.each do |site|
+  types.each {|t| Type.create!(:name => t, :site_id => site.id) }
+  topics.each {|t| Topic.create!(:name => t, :site_id => site.id) }
 end
-
-Type.create!(
- [
-   { name: 'volunteer', site_id: site.id },
-   { name: 'social', site_id: site.id },
-   { name: 'meeting', site_id: site.id },
-   { name: 'educational', site_id: site.id },
-   { name: 'rally', site_id: site.id },
-   { name: 'other', site_id: site.id },
-   { name: 'film screening', site_id: site.id }
- ])
-
- Topic.create!(
-  [
-   { name: 'animal rights', site_id: site.id },
-   { name: 'arts', site_id: site.id  },
-   { name: 'biking', site_id: site.id },
-   { name: 'business', site_id: site.id },
-   { name: 'community', site_id: site.id },
-   { name: 'urban', site_id: site.id },
-   { name: 'environment', site_id: site.id },
-   { name: 'economics', site_id: site.id },
-   { name: 'food', site_id: site.id },
-   { name: 'health', site_id: site.id },
-   { name: 'human rights', site_id: site.id },
-   { name: 'politics', site_id: site.id },
-   { name: 'transportation', site_id: site.id },
-   { name: 'walking', site_id: site.id },
-   { name: 'transit', site_id: site.id },
-   { name: 'humanitarian aid', site_id: site.id },
-   { name: 'war and peace', site_id: site.id }
-  ])
