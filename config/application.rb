f582dc7f500102e3dec153585ddccd67d0e3a3a3
Heather Moore <moore.hj@gmail.com>
2014-05-16 14:15:36 -0700

    remove i18n warning

--- config/application.rb
+++ config/application.rb
@@ -95,6 +95,7 @@ module Calagator
     # Enable traversing up the locale hierarchy until we find a match,
     # specifically: en-x-foo-bar -> en-x-foo -> en (not translation path)
     config.i18n.fallbacks = true
+    config.i18n.enforce_available_locales = true
 
     # Include subdirectories when searching for locales
     i18n_path = Rails.root.join('config', 'locales', '**', '*.{rb,yml}')
