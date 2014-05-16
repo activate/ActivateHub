871c906ef4c8465c369eee4f17b0262cc0e13769
Heather Moore <moore.hj@gmail.com>
2014-05-16 10:47:06 -0700

    render a link to login from the navbar

--- spec/views/site/_navbar.html.haml_spec.rb
+++ spec/views/site/_navbar.html.haml_spec.rb
@@ -2,7 +2,13 @@ require 'spec_helper'
 
 describe "/site/_navbar.html.haml" do
   it "renders a login link" do
+    link_class = {:events => '', :organizations => '', :venues => ''}
+    view.should_receive(:link_class).any_number_of_times.and_return(link_class)
+
+    current_site = double(:name => "foo")
+    assign(:current_site, current_site)
+
     render
-    rendered.should have_text(/Login/)
+    rendered.should include "Login"
   end
 end
