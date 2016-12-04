a295c5fc8f447dc95694d301e4af30d63458ab28
Daniel Hedlund <daniel@digitree.org>
2016-12-04 04:21:30 -0800

    Cleanup Gemfile, remove things not used by project

--- Gemfile
+++ Gemfile
@@ -1,67 +1,44 @@
-# If you are running a development environment and need to disable any
-# optional gems or add additional tools, see doc/examples/Gemfile.local
-
 source 'https://rubygems.org'
 
-# FIXME: added Aug 5, 2012 -- this may be out of date
-unless defined?($BUNDLER_INTERPRETER_CHECKED)
-  if defined?(JRUBY_VERSION)
-    puts "WARNING: JRuby cannot run Calagator. Its version of Nokogiri is incompatible with 'loofah', 'mofo' and other things. Although basic things like running the console and starting the server work, you'll run into problems as soon as you try to add/edit records or import hCalendar events."
-    $JRUBY_WARNED = true
-  end
-  $BUNDLER_INTERPRETER_CHECKED = true
+# Heroku requires ruby version on own line (reps for version, not evals)
+unless %w(true yes 1).include?(ENV['IGNORE_RUBY_VER'])
+  ruby '2.3.3'
 end
 
-#---[ Database Adapter ]----------------------------------------------------
-
-# custom Gemfile override, then env var, falling back to sqlite3
-case defined?(DB_ADAPTER) ? DB_ADAPTER : ENV['DB_ADAPTER'] || 'sqlite3'
-  when 'custom'     then # will load gems manually
-  when 'postgresql' then # will always get installed, see below
-  when 'mysql'      then gem 'mysql2'
-  when 'sqlite3'    then gem 'sqlite3'
-  else raise "unknown database adapter: #{ENV['DB_ADAPTER']}"
-end
-
-# really want 'pg' (postgresql) to stay in Gemfile.lock, for production, so
-# will only allow override if different lock file (i.e. Gemfile.local.lock)
-gem 'pg' unless defined?(DB_ADAPTER) && DB_ADAPTER != 'postgresql'
-
-#---[ Core Gems ]-----------------------------------------------------------
-
-gem 'thin'
-
-# Run-time dependencies
 gem 'rails', '~> 5.0.0'
-gem 'rails_autolink'
-gem 'nokogiri'
-gem 'columnize'
-gem 'rdoc', :require => false
-gem 'geokit'
-gem 'htmlentities'
-gem 'paper_trail'
-gem 'ri_cal'
-gem 'rubyzip'
-gem 'kaminari'
-gem 'httparty'
-gem 'loofah'
-gem 'loofah-activerecord'
-gem 'bluecloth'
-gem 'formtastic'
-gem 'acts-as-taggable-on'
-gem 'jquery-rails'
-gem 'progress_bar'
-gem 'exception_notification'
-gem 'rails3-jquery-autocomplete'
-gem 'haml'
-gem 'devise'
+gem 'puma'
+gem 'pg'
 gem 'dalli'
-gem 'font-awesome-rails'
-gem 'jquery-ui-rails'
-gem 'responders'
-gem 'activemodel-serializers-xml'
 
-gem 'rails_admin'
+gem 'geokit' # For looking up lat/long coords of venues
+gem 'paper_trail' # Change tracking of publicly editable content
+gem 'acts-as-taggable-on'
+
+# Calendar Fetching / Generating
+gem 'httparty' # Used to fetch remote calendars
+gem 'htmlentities' # Used by hcal parser
+gem 'ri_cal' # iCalendar support
+gem 'loofah' # Strip tags from descriptions when not wanting HTML
+gem 'loofah-activerecord' # Mixes in ActiveRecord::Base#xss_foliate
+gem 'nokogiri' # Used by mofo for microformat parsing
+
+# View / Rendering
+gem 'haml'
+gem 'formtastic'
+gem 'bluecloth' # Allow markdown in event/venue description
+gem 'rails_autolink' # Convert inline links into html links
+gem 'kaminari' # Pagination
+
+# Assets / Javascript
+gem 'sass-rails'
+gem 'coffee-rails'
+gem 'uglifier'
+gem 'jquery-rails'
+gem 'jquery-ui-rails'
+
+# Admin / Management
+gem 'devise' # Authentication
+gem 'rails_admin' # For creating new sites
 
 # Main gem does not yet have rails 5 support, using forked copy from PR:
 # https://github.com/fusion94/paper_trail_manager/pull/31
@@ -69,84 +46,39 @@ gem 'paper_trail_manager',
   git: 'https://github.com/kwstannard/paper_trail_manager.git',
   branch: 'rails5'
 
-platform :jruby do
-  gem 'activerecord-jdbc-adapter'
-  gem 'jruby-openssl'
-  gem 'jruby-rack'
-  gem 'warbler'
+# Legacy, to support rails/rspec upgrades
+gem 'activemodel-serializers-xml'
+gem 'responders'
 
-  gem 'activerecord-jdbcsqlite3-adapter'
-  gem 'jdbc-sqlite3'
+group :development do
+  gem 'spring'
+  gem 'spring-watcher-listen'
+  gem 'web-console'
 end
 
-platform :rbx do
-  gem "rubysl"
-  gem "racc"
-end
-
-# Some dependencies are only needed for test and development environments. On
-# production servers, you can skip their installation by running:
-#   bundle install --without development:test
 group :development, :test do
   gem 'pry-rails'
+  gem 'pry-byebug', platform: :mri
   gem 'rspec-rails'
-  gem 'test-unit'
   gem 'faker' # Used by db/seeds.rb
-  gem 'spork'
-
-# # Do not install these interactive libraries onto the continuous integration server.
-# unless ENV['CI'] || ENV['TRAVIS']
-#   # Deployment
-#   gem 'capistrano', '2.12.0'
-#   gem 'capistrano-ext', '1.2.1'
-# end
 end
 
 group :test do
-  gem 'webrat'
-  gem 'factory_girl_rails'
-  gem 'database_cleaner'
-  gem 'rspec-its'
-  gem 'rspec-activemodel-mocks'
-  gem 'rspec-collection_matchers'
   gem 'capybara'
+  gem 'factory_girl_rails'
   gem 'timecop'
-  gem 'rails-controller-testing'
 
   # Main gem does not yet have rails 5 support, using forked copy from PR:
   # https://github.com/thoughtbot/shoulda-matchers/pull/965
   gem 'shoulda-matchers',
     git: 'https://github.com/bsodmike/shoulda-matchers',
     branch: 'fix/rails5_issue_913'
+
+  # Legacy, to support rails/rspec upgrades
+  gem 'rspec-activemodel-mocks'
+  gem 'rspec-collection_matchers'
+  gem 'rails-controller-testing'
+  gem 'rspec-its'
 end
 
-group :development do
-  gem 'spring'
-  gem 'web-console'
 
-  # Better Errors (interactive web debugging)
-  gem 'better_errors'
-  gem 'binding_of_caller'
-end
-
-# Gems used only for assets and not required
-# in production environments by default.
-gem 'sass-rails', require: false
-gem 'coffee-rails', require: false
-
-# See https://github.com/sstephenson/execjs#readme for more supported runtimes
-gem 'therubyracer', platforms: :ruby, require: false
-
-# Minify assets.  Requires a javascript runtime, such as 'therubyracer'
-# above. You will also need to set 'config.assets.js_compressor' to ':uglifier'
-# in config/environments/production.rb
-gem 'uglifier', require: false
-
-# Some dependencies are activated through server settings.
-require "#{File.dirname(__FILE__)}/lib/secrets_reader"
-secrets = SecretsReader.read(:silent => true)
-case secrets.search_engine
-when 'sunspot'
-  gem 'sunspot_rails'
-  gem 'sunspot_solr'
-end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,114 +1,69 @@
 require 'rubygems'
-require 'spork'
-#uncomment the following line to use spork with the debugger
-#require 'spork/ext/ruby-debug'
 
-Spork.prefork do
-  # Loading more in this block will cause your tests to run faster. However,
-  # if you change any configuration or code from libraries loaded here, you'll
-  # need to restart spork for it take effect.
+# Calagator:
+ENV['RAILS_ENV'] = 'test' if ENV['RAILS_ENV'].to_s.empty? || ENV['RAILS_ENV'] == 'development'
 
-  # This file is copied to spec/ when you run 'rails generate rspec:install'
+require File.expand_path("../../config/environment", __FILE__)
+require 'rspec/rails'
 
-  # Calagator:
-  ENV['RAILS_ENV'] = 'test' if ENV['RAILS_ENV'].to_s.empty? || ENV['RAILS_ENV'] == 'development'
+# Requires supporting ruby files with custom matchers and macros, etc,
+# in spec/support/ and its subdirectories.
+Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
 
-  require File.expand_path("../../config/environment", __FILE__)
-  require 'rspec/rails'
+# Calagator: Load this project's custom spec extensions:
+require File.expand_path(File.dirname(__FILE__) + '/spec_helper_extensions.rb')
 
-  # Requires supporting ruby files with custom matchers and macros, etc,
-  # in spec/support/ and its subdirectories.
-  Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
+RSpec.configure do |config|
+  # == Mock Framework
+  #
+  # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
+  #
+  # config.mock_with :mocha
+  # config.mock_with :flexmock
+  # config.mock_with :rr
+  config.mock_with :rspec
 
-  # Calagator: Load this project's custom spec extensions:
-  require File.expand_path(File.dirname(__FILE__) + '/spec_helper_extensions.rb')
+  # Filter out gems from backtraces
+  config.backtrace_exclusion_patterns << /vendor\//
+  config.backtrace_exclusion_patterns << /lib\/rspec\/rails/
+  config.backtrace_exclusion_patterns << /gems\//
 
-  RSpec.configure do |config|
-    # == Mock Framework
-    #
-    # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
-    #
-    # config.mock_with :mocha
-    # config.mock_with :flexmock
-    # config.mock_with :rr
-    config.mock_with :rspec
+  config.use_transactional_fixtures = true
 
-    # Filter out gems from backtraces
-    config.backtrace_exclusion_patterns << /vendor\//
-    config.backtrace_exclusion_patterns << /lib\/rspec\/rails/
-    config.backtrace_exclusion_patterns << /gems\//
+  # Allows us to use create(:user) instead of FactoryGirl.create :user
+  config.include FactoryGirl::Syntax::Methods
 
-    config.use_transactional_fixtures = true
+  config.include Shoulda::Matchers::ActiveModel, type: :model
+  config.include Shoulda::Matchers::ActiveRecord, type: :model
 
-    # Allows us to use create(:user) instead of FactoryGirl.create :user
-    config.include FactoryGirl::Syntax::Methods
+  # Makes warden available in tests, provides :sign_in and :sign_out
+  config.include Devise::Test::ControllerHelpers, :type => :controller
+  config.include Devise::Test::IntegrationHelpers, :type => :request
 
-    config.include Shoulda::Matchers::ActiveModel, type: :model
-    config.include Shoulda::Matchers::ActiveRecord, type: :model
+  # Some of the controller specs are using capybara-based matchers
+  config.include Capybara::RSpecMatchers, :type => :controller
 
-    # Makes warden available in tests, provides :sign_in and :sign_out
-    config.include Devise::Test::ControllerHelpers, :type => :controller
-    config.include Devise::Test::IntegrationHelpers, :type => :request
+  # custom helpers and mixins, see spec/support/*
+  config.include ControllerHelper, :type => :controller
 
-    # Some of the controller specs are using capybara-based matchers
-    config.include Capybara::RSpecMatchers, :type => :controller
+  # Database cleaner
+  config.before(:suite) do
+    # use a fixed time so tests and fixtures can make assumptions
+    # about future events and not worry about changes in seconds, etc
+    Timecop.travel(Time.zone.parse('2013-03-22 14:05:27'))
+    Timecop.safe_mode = true
+  end
 
-    # custom helpers and mixins, see spec/support/*
-    config.include ControllerHelper, :type => :controller
-
-    # Database cleaner
-    config.before(:suite) do
-      # use a fixed time so tests and fixtures can make assumptions
-      # about future events and not worry about changes in seconds, etc
-      Timecop.travel(Time.zone.parse('2013-03-22 14:05:27'))
-      Timecop.safe_mode = true
-    end
-
-    config.before(:each) do
-      # data is tenantized by site, so we need to ensure a site exists for
-      # all tests and that it matches the request.domain used for controller
-      # and functional tests.
-      ENV['TEST_REQ_HOST'] = 'activate.test'
-      Site.create(
-        :name     => 'Test Site',
-        :domain   => ENV['TEST_REQ_HOST'],
-        :timezone => 'Pacific Time (US & Canada)',
-        :locale   => 'en',
-      ).use!
-    end
+  config.before(:each) do
+    # data is tenantized by site, so we need to ensure a site exists for
+    # all tests and that it matches the request.domain used for controller
+    # and functional tests.
+    ENV['TEST_REQ_HOST'] = 'activate.test'
+    Site.create(
+      :name     => 'Test Site',
+      :domain   => ENV['TEST_REQ_HOST'],
+      :timezone => 'Pacific Time (US & Canada)',
+      :locale   => 'en',
+    ).use!
   end
 end
-
-Spork.each_run do
-  # This code will be run each time you run your specs.
-
-end
-
-# --- Instructions ---
-# Sort the contents of this file into a Spork.prefork and a Spork.each_run
-# block.
-#
-# The Spork.prefork block is run only once when the spork server is started.
-# You typically want to place most of your (slow) initializer code in here, in
-# particular, require'ing any 3rd-party gems that you don't normally modify
-# during development.
-#
-# The Spork.each_run block is run each time you run your specs.  In case you
-# need to load files that tend to change during development, require them here.
-# With Rails, your application modules are loaded automatically, so sometimes
-# this block can remain empty.
-#
-# Note: You can modify files loaded *from* the Spork.each_run block without
-# restarting the spork server.  However, this file itself will not be reloaded,
-# so if you change any of the code inside the each_run block, you still need to
-# restart the server.  In general, if you have non-trivial code in this file,
-# it's advisable to move it into a separate file so you can easily edit it
-# without restarting spork.  (For example, with RSpec, you could move
-# non-trivial code into a file spec/support/my_helper.rb, making sure that the
-# spec/support/* files are require'd from inside the each_run block.)
-#
-# Any code that is left outside the two blocks will be run during preforking
-# *and* during each_run -- that's probably not what you want.
-#
-# These instructions should self-destruct in 10 seconds.  If they don't, feel
-# free to delete them.
