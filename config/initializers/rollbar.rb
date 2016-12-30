6b5226bcd01b3054ff6157eba8da7218751292e4
Daniel Hedlund <daniel@digitree.org>
2016-12-30 00:30:13 -0800

    Have rollbar ignore routing and unknown format errors

--- config/initializers/rollbar.rb
+++ config/initializers/rollbar.rb
@@ -31,6 +31,10 @@ Rollbar.configure do |config|
   #
   # You can also specify a callable, which will be called with the exception instance.
   # config.exception_level_filters.merge!('MyCriticalException' => lambda { |e| 'critical' })
+  config.exception_level_filters.merge!(
+    'ActionController::RoutingError' => 'ignore',
+    'ActionController::UnknownFormat' => 'ignore',
+  )
 
   # Enable asynchronous reporting (uses girl_friday or Threading if girl_friday
   # is not installed)
